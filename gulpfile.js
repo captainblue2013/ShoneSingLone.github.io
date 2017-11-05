9a159f4145981c5f6ab353c4f5cd8675e2acc259
ShoneSingLone <ShoneSingLone@gmail.com>
2017-11-05 22:31:07 +0800

    【update】

--- gulpfile.js
+++ gulpfile.js
@@ -44,3 +44,18 @@ gulp.task('css', function () {
         .pipe(gulp.dest('public/bootstrap/css'));
 });
 
+
+var bs = require('browser-sync').create();
+
+gulp.task('serve', [], () => {
+    bs.init({
+        server: {
+            baseDir: "./",
+        },
+        port: 5000
+        // reloadOnRestart: true,
+        // browser: "google chrome"
+    });
+    gulp.watch('./*.html', ['', bs.reload]);
+});
+

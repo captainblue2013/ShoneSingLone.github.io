c34c7da9add8492999131515cd713d9581f6ab36
ShoneSingLone <ShoneSingLone@gmail.com>
2018-03-24 22:44:17 +0800

    【add】body

--- my-github-page/src/pages/home/components/HomePage.vue
+++ my-github-page/src/pages/home/components/HomePage.vue
@@ -3,62 +3,36 @@
     <nav-top-bar slot="header" />
     <main slot="main" id="main-content" class="container">
       <!-- container start -->
-      <div class="row">
-        <!-- row start -->
-        <div class="col-sm-8">
-          <div class="panel panel-default">
-            <div class="panel-heading">
-              <h2 class="" class="mdl-card__title-text">WII-chat</h2>
-            </div>
-            <div class="panel-body">
-              <div class="mdl-card__supporting-text">learn WebSocket：a online multi-user chatroom based on socket.io
-                <p>感谢
-                  <a href="https://leancloud.cn/">LeanCloud</a>提供的
-                  <em>体验实例</em>，可以部署Node 服务端应用，而且提供HTTPS。
-                  <em> 尽管有
-                    <a href="https://leancloud.cn/docs/leanengine_plan.html#hash633315134">休眠策略</a>，
-                    <a href="https://leancloud.cn/docs/leanengine_plan.html#%E4%BC%91%E7%9C%A0%E7%AD%96%E7%95%A5">每天 6 小时的强制休眠时间</a>
-                  </em> 有概率用不了( ･´ω`･ ) </p>
-              </div>
-            </div>
-            <div class="panel-footer">
-              <a class="btn btn-default" target="_blank" href="./chat.html" role="button">wii-chat»</a>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-4">
-          <div class="panel panel-default">
-            <div class="panel-heading">
-              <h2 class="" class="mdl-card__title-text">elem-app</h2>
-            </div>
-            <div class="panel-body">
-              <div class="mdl-card__supporting-text">Vue.js高仿饿了么外卖App 1.0到2.0版本完美升级
-                <a target="_blank" href="http://coding.imooc.com/class/74.html">mooc</a>
-              </div>
-            </div>
-            <div class="panel-footer">
-              <a class="btn btn-default" target="_blank" href="./dist/index.html" role="button">elem-app»</a>
-              </a>
-              <!-- <a class="btn btn-default" target="_blank" href="https://shonesinglone.github.io/mindmap/project/VuePractice/elem-app/dist/#/goods" role="button">elem-app»</a> </a> -->
-            </div>
-          </div>
-        </div>
-        <!-- row end -->
+      <div class="row" 　v-for="portfolio in portfolioArray">
+        <panel-card :portfolio="portfolio" />
       </div>
-      <div id="main-coontainer-row" class="row">
-        <!-- row start -->
+
+      <!-- row start -->
+
+      <!--  
         <div class="col-sm-4">
-          <div id="main-container-row-col1"></div>
-        </div>
-        <div class="col-sm-4">
-          <div id="main-container-row-col2"></div>
-        </div>
-        <div class="col-sm-4">
-          <div id="main-container-row-col3"></div>
-        </div>
-        <!-- row end -->
-      </div>
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h2 class="" class="mdl-card__title-text">elem-app</h2>
+                        </div>
+                        <div class="panel-body">
+                            <div class="mdl-card__supporting-text">Vue.js高仿饿了么外卖App 1.0到2.0版本完美升级
+                                <a target="_blank" href="http://coding.imooc.com/class/74.html">mooc</a>
+                            </div>
+                        </div>
+                        <div class="panel-footer">
+                            <a class="btn btn-default" target="_blank" href="./dist/index.html" role="button">elem-app»</a>
+                            </a>
+                            <a class="btn btn-default" target="_blank" href="https://shonesinglone.github.io/mindmap/project/VuePractice/elem-app/dist/#/goods"
+                                role="button">elem-app»</a>
+                            </a>
+                        </div>
+                    </div>
+                </div> 
+                -->
+
+      <!-- row end -->
+
       <!-- container end -->
     </main>
     <footer id="footer" slot="footer">
@@ -83,16 +57,38 @@
 <script>
 import LayoutNormal from "@cc/LayoutNormal";
 import NavTopBar from "./NavTopBar";
+import PanelCard from "./PanelCard";
 
 export default {
   name: "HomePage",
   data() {
     return {
-      msg: "Welcome to Your Vue.js App"
+      msg: "Welcome to Your Vue.js App",
+      portfolioArray: [
+        { heading: { content: "WII-chat", url: "" }, body: { content: 'learn WebSocket：a online multi-user chatroom based on socket.io <p>感谢 <a href="https://leancloud.cn/">LeanCloud</a>提供的 <em>体验实例</em>，可以部署Node 服务端应用，而且提供HTTPS。 <em> 尽管有 <a href="https://leancloud.cn/docs/leanengine_plan.html#hash633315134">休眠策略</a>， <a href="https://leancloud.cn/docs/leanengine_plan.html#%E4%BC%91%E7%9C%A0%E7%AD%96%E7%95%A5">每天 6 小时的强制休眠时间</a> </em> 有概率用不了( ･´ω`･ ) </p>' }, footer: { content: '<a class="btn btn-default" target="_blank" href="./chat.html" role="button">wii-chat»</a> </a>' } },
+        { heading: { content: "WII-chat", url: "" }, body: { content: 'learn WebSocket：a online multi-user chatroom based on socket.io <p>感谢 <a href="https://leancloud.cn/">LeanCloud</a>提供的 <em>体验实例</em>，可以部署Node 服务端应用，而且提供HTTPS。 <em> 尽管有 <a href="https://leancloud.cn/docs/leanengine_plan.html#hash633315134">休眠策略</a>， <a href="https://leancloud.cn/docs/leanengine_plan.html#%E4%BC%91%E7%9C%A0%E7%AD%96%E7%95%A5">每天 6 小时的强制休眠时间</a> </em> 有概率用不了( ･´ω`･ ) </p>' }, footer: { content: '<a class="btn btn-default" target="_blank" href="./chat.html" role="button">wii-chat»</a> </a>' } },
+        { heading: { content: "WII-chat", url: "" }, body: { content: 'learn WebSocket：a online multi-user chatroom based on socket.io <p>感谢 <a href="https://leancloud.cn/">LeanCloud</a>提供的 <em>体验实例</em>，可以部署Node 服务端应用，而且提供HTTPS。 <em> 尽管有 <a href="https://leancloud.cn/docs/leanengine_plan.html#hash633315134">休眠策略</a>， <a href="https://leancloud.cn/docs/leanengine_plan.html#%E4%BC%91%E7%9C%A0%E7%AD%96%E7%95%A5">每天 6 小时的强制休眠时间</a> </em> 有概率用不了( ･´ω`･ ) </p>' }, footer: { content: '<a class="btn btn-default" target="_blank" href="./chat.html" role="button">wii-chat»</a> </a>' } },
+        { heading: { content: "WII-chat", url: "" }, body: { content: 'learn WebSocket：a online multi-user chatroom based on socket.io <p>感谢 <a href="https://leancloud.cn/">LeanCloud</a>提供的 <em>体验实例</em>，可以部署Node 服务端应用，而且提供HTTPS。 <em> 尽管有 <a href="https://leancloud.cn/docs/leanengine_plan.html#hash633315134">休眠策略</a>， <a href="https://leancloud.cn/docs/leanengine_plan.html#%E4%BC%91%E7%9C%A0%E7%AD%96%E7%95%A5">每天 6 小时的强制休眠时间</a> </em> 有概率用不了( ･´ω`･ ) </p>' }, footer: { content: '<a class="btn btn-default" target="_blank" href="./chat.html" role="button">wii-chat»</a> </a>' } },
+        { heading: { content: "WII-chat", url: "" }, body: { content: 'learn WebSocket：a online multi-user chatroom based on socket.io <p>感谢 <a href="https://leancloud.cn/">LeanCloud</a>提供的 <em>体验实例</em>，可以部署Node 服务端应用，而且提供HTTPS。 <em> 尽管有 <a href="https://leancloud.cn/docs/leanengine_plan.html#hash633315134">休眠策略</a>， <a href="https://leancloud.cn/docs/leanengine_plan.html#%E4%BC%91%E7%9C%A0%E7%AD%96%E7%95%A5">每天 6 小时的强制休眠时间</a> </em> 有概率用不了( ･´ω`･ ) </p>' }, footer: { content: '<a class="btn btn-default" target="_blank" href="./chat.html" role="button">wii-chat»</a> </a>' } },
+        { heading: { content: "WII-chat", url: "" }, body: { content: 'learn WebSocket：a online multi-user chatroom based on socket.io <p>感谢 <a href="https://leancloud.cn/">LeanCloud</a>提供的 <em>体验实例</em>，可以部署Node 服务端应用，而且提供HTTPS。 <em> 尽管有 <a href="https://leancloud.cn/docs/leanengine_plan.html#hash633315134">休眠策略</a>， <a href="https://leancloud.cn/docs/leanengine_plan.html#%E4%BC%91%E7%9C%A0%E7%AD%96%E7%95%A5">每天 6 小时的强制休眠时间</a> </em> 有概率用不了( ･´ω`･ ) </p>' }, footer: { content: '<a class="btn btn-default" target="_blank" href="./chat.html" role="button">wii-chat»</a> </a>' } },
+        { heading: { content: "WII-chat", url: "" }, body: { content: 'learn WebSocket：a online multi-user chatroom based on socket.io <p>感谢 <a href="https://leancloud.cn/">LeanCloud</a>提供的 <em>体验实例</em>，可以部署Node 服务端应用，而且提供HTTPS。 <em> 尽管有 <a href="https://leancloud.cn/docs/leanengine_plan.html#hash633315134">休眠策略</a>， <a href="https://leancloud.cn/docs/leanengine_plan.html#%E4%BC%91%E7%9C%A0%E7%AD%96%E7%95%A5">每天 6 小时的强制休眠时间</a> </em> 有概率用不了( ･´ω`･ ) </p>' }, footer: { content: '<a class="btn btn-default" target="_blank" href="./chat.html" role="button">wii-chat»</a> </a>' } },
+        {
+          heading: { content: "WII-chat", url: "" },
+          body: {
+            content:
+              'learn WebSocket：a online multi-user chatroom based on socket.io <p>感谢 <a href="https://leancloud.cn/">LeanCloud</a>提供的 <em>体验实例</em>，可以部署Node 服务端应用，而且提供HTTPS。 <em> 尽管有 <a href="https://leancloud.cn/docs/leanengine_plan.html#hash633315134">休眠策略</a>， <a href="https://leancloud.cn/docs/leanengine_plan.html#%E4%BC%91%E7%9C%A0%E7%AD%96%E7%95%A5">每天 6 小时的强制休眠时间</a> </em> 有概率用不了( ･´ω`･ ) </p>'
+          },
+          footer: {
+            content:
+              '<a class="btn btn-default" target="_blank" href="./chat.html" role="button">wii-chat»</a> </a>'
+          }
+        }
+      ]
     };
   },
   components: {
     "layout-normal": LayoutNormal,
+    "panel-card": PanelCard,
     "nav-top-bar": NavTopBar
   }
 };
---
+++ my-github-page/src/pages/home/components/PanelCard.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="col-sm-8">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h2 class="" class="heading-text">{{hContent}}</h2>
+      </div>
+      <div class="panel-body">
+        <div class="body-text">
+          {{bContent}}
+        </div>
+      </div>
+      <div class="panel-footer">
+        {{fContent}}
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script type="text/ecmascript-6">
+// import BSModal from "@cc/bootstrap/Modal";
+
+export default {
+  name: "PanelCard",
+  props: {
+    portfolio: {
+      type: Object,
+      required: true,
+      default: {}
+    }
+  },
+  data() {
+    return {
+      /* 
+      anchorArray,
+      whichActiveAnchor: 0
+     */
+    };
+  },
+  components: {
+    /* 
+    // "bs-modal": BSModal
+   */
+  },
+  methods: {
+    getHTML(htmlString) {
+      let html = this.jQuery(htmlString);
+      console.log(html);
+      return html;
+    }
+    /* 
+    setActiveAnchor(index) {
+      this.whichActiveAnchor = index;
+    }
+   */
+  },
+  computed: {
+    hContent() {
+      return this.portfolio.heading.content;
+    },
+    bContent() {
+      return this.portfolio.body.content;
+    },
+    fContent() {
+      return this.portfolio.footer.content;
+    }
+    /* 
+    isShowNav() {
+      if (!this.isMobile) return true;
+      return this.$store.state.mainState.isShowModal;
+    },
+    isShowModal() {
+      return this.$store.state.mainState.isShowModal && this.isMobile;
+    }
+   */
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "../../../common/scss/bootstrap/common";
+@import "../../../common/scss/my_variables";
+
+$border-bottom-height: 0.4rem;
+$border-radius: 1rem;
+</style>

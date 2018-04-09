f7c406337a9baf7a00a0876bc6a82fce9d3f3cb6
ShoneSingLone <ShoneSingLone@gmail.com>
2018-04-09 22:54:18 +0800

    【test】change the bloglist origin

--- my-github-page/src/store/modules/blogState.js
+++ my-github-page/src/store/modules/blogState.js
@@ -7,7 +7,7 @@ import Vue from 'vue';
 
 function BlogCard(
   heading = "WII-chat",
-  body = 'learn WebSocket：a online multi-user chatroom based on socket.io <p>感谢 <a href="https://leancloud.cn/">LeanCloud</a>提供的 <em>体验实例</em>，可以部署Node 服务端应用，而且提供HTTPS。 <em> 尽管有 <a href="https://leancloud.cn/docs/leanengine_plan.html#hash633315134">休眠策略</a>， <a href="https://leancloud.cn/docs/leanengine_plan.html#%E4%BC%91%E7%9C%A0%E7%AD%96%E7%95%A5">每天 6 小时的强制休眠时间</a> </em> 有概率用不了( ･´ω`･ ) </p>',
+  body = 'learn WebSocket：a online multi-user chatroom based on socket.io <p>利用 <a href="https://leancloud.cn/">LeanCloud</a>提供的 <em>体验实例</em>，部署Node 服务端应用。 <em> 尽管有 <a href="https://leancloud.cn/docs/leanengine_plan.html#hash633315134">休眠策略</a>， <a href="https://leancloud.cn/docs/leanengine_plan.html#%E4%BC%91%E7%9C%A0%E7%AD%96%E7%95%A5">每天 6 小时的强制休眠时间</a> </em> 有概率用不了( ･´ω`･ ) ，但是提供HTTPS，很棒。因为我懒。</p>',
   footer = '<a class="btn btn-default" target="_blank" href="./chat.html" role="button">wii-chat»</a> </a>'
 ) {
   this.heading = {

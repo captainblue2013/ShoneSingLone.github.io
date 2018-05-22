a00a72cafd5fd678b31de9d0561fe95174fcc198
ShoneSingLone <ShoneSingLone@gmail.com>
2018-05-22 22:37:47 +0800

    【update】

--- my-github-page/src/pages/home/components/PanelLogin.vue
+++ my-github-page/src/pages/home/components/PanelLogin.vue
@@ -24,6 +24,7 @@ export default {
       let vue = this;
       vue.axios
         .post(window.remotHost + "p/api", {
+          type:"login",
           user: this.user
         })
         .then(response => {

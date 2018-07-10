a36c2ed0548f018da6ddc1cea7fe3005701171b9
ShoneSingLone <ShoneSingLone@gmail.com>
2018-07-10 14:54:32 +0800

    updata 解决messidor 静态资源路径以及自动copy脚本问题

--- messidor/service-worker.js
+++ messidor/service-worker.js
@@ -1,4 +1,4 @@
-importScripts('/messidorstatic/js/workbox-sw.prod.v2.1.3.js');/**
+importScripts('/messidor/static/js/workbox-sw.prod.v2.1.3.js');/**
  * @file service-worker.js with workbox api
  * @desc [example](https://workbox-samples.glitch.me/examples/workbox-sw/)
  * @author ShoneSingLone(ShoneSingLone@gmail.com)
@@ -16,45 +16,45 @@ const workboxSW = new WorkboxSW({
 // Define precache injection point.
 workboxSW.precache([
   {
-    "url": "/index.html",
-    "revision": "1b1faca5672f274a4211d46bbfc7cf1c"
+    "url": "/messidor/index.html",
+    "revision": "925f30f7b464244f5a44619e11ede922"
   },
   {
-    "url": "/static/css/index.ba3d0f07.css"
+    "url": "/messidor/static/css/index.56b39a66.css"
   },
   {
-    "url": "/static/fonts/MaterialIcons-Regular.012cf6a1.woff"
+    "url": "/messidor/static/fonts/MaterialIcons-Regular.012cf6a1.woff"
   },
   {
-    "url": "/static/fonts/MaterialIcons-Regular.a37b0c01.ttf"
+    "url": "/messidor/static/fonts/MaterialIcons-Regular.a37b0c01.ttf"
   },
   {
-    "url": "/static/js/0.77ba67d1.js"
+    "url": "/messidor/static/js/0.77ba67d1.js"
   },
   {
-    "url": "/static/js/1.ca373c1a.js"
+    "url": "/messidor/static/js/1.ca373c1a.js"
   },
   {
-    "url": "/static/js/2.f0e2d716.js"
+    "url": "/messidor/static/js/2.f0e2d716.js"
   },
   {
-    "url": "/static/js/index.f52b4f3a.js"
+    "url": "/messidor/static/js/index.cc0c39ab.js"
   },
   {
-    "url": "/static/js/manifest.b8fd4135.js"
+    "url": "/messidor/static/js/manifest.519492a5.js"
   },
   {
-    "url": "/static/js/vendor.194de34c.js"
+    "url": "/messidor/static/js/vendor.194de34c.js"
   },
   {
-    "url": "/static/js/vue.f62a68f9.js"
+    "url": "/messidor/static/js/vue.f62a68f9.js"
   },
   {
-    "url": "/static/js/workbox-sw.prod.v2.1.3.js",
+    "url": "/messidor/static/js/workbox-sw.prod.v2.1.3.js",
     "revision": "a9890beda9e5f17e4c68f42324217941"
   }
 ]);
-workboxSW.router.registerNavigationRoute('/messidorindex.html');
+workboxSW.router.registerNavigationRoute('/messidor/index.html');
 
 
 // Define runtime cache.

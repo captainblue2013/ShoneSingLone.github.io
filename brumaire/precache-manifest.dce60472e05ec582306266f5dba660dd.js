44c6cf2ca26c8f2e8112c29197da8ba22394735d
ShoneSingLone <ShoneSingLone@gmail.com>
2018-09-06 23:51:35 +0800

    update

---
+++ brumaire/precache-manifest.dce60472e05ec582306266f5dba660dd.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = [
+  {
+    "revision": "c84ea466198d83796078",
+    "url": "/brumaire/js/checkout.aaf1e651.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/brumaire/robots.txt"
+  },
+  {
+    "revision": "2087250dd7458af70c3c",
+    "url": "/brumaire/css/chunk-vendors.a228a942.css"
+  },
+  {
+    "revision": "057446fe29d49a0bbbf1",
+    "url": "/brumaire/css/article.71d627e9.css"
+  },
+  {
+    "revision": "79c7574f3e5658019a6d",
+    "url": "/brumaire/css/c-card.e7449773.css"
+  },
+  {
+    "revision": "79c7574f3e5658019a6d",
+    "url": "/brumaire/js/c-card.0f656df1.js"
+  },
+  {
+    "revision": "71c562679fccf3ea44af",
+    "url": "/brumaire/js/c-col.37372399.js"
+  },
+  {
+    "revision": "08be07fcc09c61e09192",
+    "url": "/brumaire/js/c-components.76edddc7.js"
+  },
+  {
+    "revision": "8f8a055f8fe370fbd6f6",
+    "url": "/brumaire/js/c-container.43a9e703.js"
+  },
+  {
+    "revision": "ec8515fd1dfc4ff25246",
+    "url": "/brumaire/css/c-input.612fe381.css"
+  },
+  {
+    "revision": "ec8515fd1dfc4ff25246",
+    "url": "/brumaire/js/c-input.78e11f79.js"
+  },
+  {
+    "revision": "497d96b11e841e5687bb",
+    "url": "/brumaire/css/c-modals.449253e1.css"
+  },
+  {
+    "revision": "497d96b11e841e5687bb",
+    "url": "/brumaire/js/c-modals.5a98e907.js"
+  },
+  {
+    "revision": "1543fcc9bec412626265",
+    "url": "/brumaire/css/c-progress.cfb73d6c.css"
+  },
+  {
+    "revision": "1543fcc9bec412626265",
+    "url": "/brumaire/js/c-progress.729f92aa.js"
+  },
+  {
+    "revision": "ea2bf4a9406ced6021e1",
+    "url": "/brumaire/js/c-row.bda314da.js"
+  },
+  {
+    "revision": "10b77387d6d4b1107bbc",
+    "url": "/brumaire/js/chat.c8141091.js"
+  },
+  {
+    "revision": "c84ea466198d83796078",
+    "url": "/brumaire/css/checkout.e0d9363b.css"
+  },
+  {
+    "revision": "f42a88e0389a6a03f695",
+    "url": "/brumaire/js/app.a7f82627.js"
+  },
+  {
+    "revision": "057446fe29d49a0bbbf1",
+    "url": "/brumaire/js/article.d671d101.js"
+  },
+  {
+    "revision": "2087250dd7458af70c3c",
+    "url": "/brumaire/js/chunk-vendors.47cb30bd.js"
+  },
+  {
+    "revision": "4596baff488d4ce164c7",
+    "url": "/brumaire/css/error.4289f0a3.css"
+  },
+  {
+    "revision": "4596baff488d4ce164c7",
+    "url": "/brumaire/js/error.f5d35329.js"
+  },
+  {
+    "revision": "08270cb8453c74ff3c5a",
+    "url": "/brumaire/css/home.a8be767e.css"
+  },
+  {
+    "revision": "08270cb8453c74ff3c5a",
+    "url": "/brumaire/js/home.77270d80.js"
+  },
+  {
+    "revision": "511c5be65486582f0ea6",
+    "url": "/brumaire/js/question.a2cea634.js"
+  },
+  {
+    "revision": "0dab81194ad0c194cbaa",
+    "url": "/brumaire/css/user.f2969f86.css"
+  },
+  {
+    "revision": "0dab81194ad0c194cbaa",
+    "url": "/brumaire/js/user.aaddde21.js"
+  },
+  {
+    "revision": "8a5b6ae1cb47782ad1daf17ad95394d6",
+    "url": "/brumaire/img/logo.8a5b6ae1.svg"
+  },
+  {
+    "revision": "fbdbc0982da413d383090d153aaef630",
+    "url": "/brumaire/img/github.fbdbc098.svg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/brumaire/fonts/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/brumaire/img/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/brumaire/fonts/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/brumaire/fonts/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/brumaire/fonts/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "1a8066d99cb61335293738ad4d420e26",
+    "url": "/brumaire/index.html"
+  },
+  {
+    "revision": "f42a88e0389a6a03f695",
+    "url": "/brumaire/css/app.40005e80.css"
+  }
+];
--- brumaire/service-worker.js
+++ brumaire/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
 
 importScripts(
-  "/brumaire/precache-manifest.e88e11f36c19054657ccda17d0f0e0e7.js"
+  "/brumaire/precache-manifest.dce60472e05ec582306266f5dba660dd.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "Brumaire"});

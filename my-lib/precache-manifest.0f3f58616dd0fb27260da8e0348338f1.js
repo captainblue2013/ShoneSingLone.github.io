a4ae3792efbee1ea5bd73d1038ebb98585a994e4
ShoneSingLone <ShoneSingLone@gmail.com>
2019-07-09 12:33:23 +0800

    add my-lib

---
+++ my-lib/precache-manifest.0f3f58616dd0fb27260da8e0348338f1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "57bdc9c334b124ee35e8",
+    "url": "/my-lib/js/PageImgViewer.7b5be824.js"
+  },
+  {
+    "revision": "1b93d5f7b3359b285383",
+    "url": "/my-lib/js/PageLoopRoll.4b9ec1e5.js"
+  },
+  {
+    "revision": "422ad82591afb555f143",
+    "url": "/my-lib/js/about.7aedfc12.js"
+  },
+  {
+    "revision": "1de3bceca65248deb4a0",
+    "url": "/my-lib/js/app.b183111b.js"
+  },
+  {
+    "revision": "d2c5b767d91db592e28d",
+    "url": "/my-lib/js/chunk-3e43e69b.c2430c4a.js"
+  },
+  {
+    "revision": "c07cfa108cfb3d180b36",
+    "url": "/my-lib/js/chunk-vendors.0880ee80.js"
+  },
+  {
+    "revision": "bbb2cca40b95a207d95d46dad4ebdbe6",
+    "url": "/my-lib/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/my-lib/robots.txt"
+  }
+];

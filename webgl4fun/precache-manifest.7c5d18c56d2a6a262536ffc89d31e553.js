a56a5164ea97dbe56f5f29eeb8a083d17dd67dde
ShoneSingLone <ShoneSingLone@gmail.com>
2019-09-08 20:21:12 +0800

    add webgl4fun

---
+++ webgl4fun/precache-manifest.7c5d18c56d2a6a262536ffc89d31e553.js
@@ -0,0 +1,386 @@
+self.__precacheManifest = [
+  {
+    "revision": "65091535a1dda76cf306",
+    "url": "/css/Animation.6f48092a.css"
+  },
+  {
+    "revision": "65091535a1dda76cf306",
+    "url": "/js/Animation.125304b7.js"
+  },
+  {
+    "revision": "bb284dd80de039300f21",
+    "url": "/js/Chapter1.c9d6317c.js"
+  },
+  {
+    "revision": "cd870627eb1898e08cde",
+    "url": "/css/Chapter2.800e6640.css"
+  },
+  {
+    "revision": "cd870627eb1898e08cde",
+    "url": "/js/Chapter2.d8d312b6.js"
+  },
+  {
+    "revision": "20cc2fb3ece636897339",
+    "url": "/js/Chapter2.example1.ee52ef2f.js"
+  },
+  {
+    "revision": "a37dc52958d7d9712860",
+    "url": "/css/Chapter2.example2.08765a0c.css"
+  },
+  {
+    "revision": "a37dc52958d7d9712860",
+    "url": "/js/Chapter2.example2.164d2474.js"
+  },
+  {
+    "revision": "312c468f8bf14a1d0ec1",
+    "url": "/css/Home.example2.6fd41f5f.css"
+  },
+  {
+    "revision": "312c468f8bf14a1d0ec1",
+    "url": "/js/Home.example2.e2aaf72f.js"
+  },
+  {
+    "revision": "310b6bb7a320b54b4430",
+    "url": "/js/Home.example2~Home.register~ThreeJS.example2.5332d69f.js"
+  },
+  {
+    "revision": "eb6e5cd2648df826810a",
+    "url": "/css/Home.flexiblePanel.ca59ec3e.css"
+  },
+  {
+    "revision": "eb6e5cd2648df826810a",
+    "url": "/js/Home.flexiblePanel.58095c1b.js"
+  },
+  {
+    "revision": "fe2231dffcda1b1e5fa9",
+    "url": "/css/Home.register.03e8e5d7.css"
+  },
+  {
+    "revision": "fe2231dffcda1b1e5fa9",
+    "url": "/js/Home.register.49400aef.js"
+  },
+  {
+    "revision": "cc34047c27e73f2f502f",
+    "url": "/css/SVG.2919276a.css"
+  },
+  {
+    "revision": "cc34047c27e73f2f502f",
+    "url": "/js/SVG.44e7d15c.js"
+  },
+  {
+    "revision": "de338b29dd31560aa91d",
+    "url": "/css/TableExample.42f67237.css"
+  },
+  {
+    "revision": "de338b29dd31560aa91d",
+    "url": "/js/TableExample.ed572f07.js"
+  },
+  {
+    "revision": "a68ce56266b2a5fbb89a",
+    "url": "/js/TableExample~about.130aeab4.js"
+  },
+  {
+    "revision": "ce159a85937aa0b77dcc",
+    "url": "/css/ThreeJS.9f1063c5.css"
+  },
+  {
+    "revision": "ce159a85937aa0b77dcc",
+    "url": "/js/ThreeJS.1119b447.js"
+  },
+  {
+    "revision": "5adc14bca78db438d7bf",
+    "url": "/css/ThreeJS.example1.02005cc1.css"
+  },
+  {
+    "revision": "5adc14bca78db438d7bf",
+    "url": "/js/ThreeJS.example1.39fcbf30.js"
+  },
+  {
+    "revision": "16541548d794221f6fd5",
+    "url": "/css/ThreeJS.example2.02005cc1.css"
+  },
+  {
+    "revision": "16541548d794221f6fd5",
+    "url": "/js/ThreeJS.example2.e707eae2.js"
+  },
+  {
+    "revision": "1f906aa27125f101ea45",
+    "url": "/js/ThreeJS.example2~ThreeJS.example5~ThreeJS.example6~about.f165fe27.js"
+  },
+  {
+    "revision": "eac9b6bf8f9ffb9ef472",
+    "url": "/css/ThreeJS.example3.02005cc1.css"
+  },
+  {
+    "revision": "eac9b6bf8f9ffb9ef472",
+    "url": "/js/ThreeJS.example3.4c926d83.js"
+  },
+  {
+    "revision": "1d47a9462d4e907e19c9",
+    "url": "/css/ThreeJS.example4.02005cc1.css"
+  },
+  {
+    "revision": "1d47a9462d4e907e19c9",
+    "url": "/js/ThreeJS.example4.9f3cf1a4.js"
+  },
+  {
+    "revision": "ce42d761cd272ebee922",
+    "url": "/css/ThreeJS.example5.02005cc1.css"
+  },
+  {
+    "revision": "ce42d761cd272ebee922",
+    "url": "/js/ThreeJS.example5.a1054619.js"
+  },
+  {
+    "revision": "8f0d107cf809d69dcf44",
+    "url": "/css/ThreeJS.example6.02005cc1.css"
+  },
+  {
+    "revision": "8f0d107cf809d69dcf44",
+    "url": "/js/ThreeJS.example6.1866f095.js"
+  },
+  {
+    "revision": "f14695f4e7123fb9e6ba",
+    "url": "/css/ThreeJS.example7.02005cc1.css"
+  },
+  {
+    "revision": "f14695f4e7123fb9e6ba",
+    "url": "/js/ThreeJS.example7.d70aab3c.js"
+  },
+  {
+    "revision": "7f9c7d0f60919801c776",
+    "url": "/css/ThreeJS.example8.bc802572.css"
+  },
+  {
+    "revision": "7f9c7d0f60919801c776",
+    "url": "/js/ThreeJS.example8.a5e172b9.js"
+  },
+  {
+    "revision": "c872af0f43aab5a2ac26",
+    "url": "/css/about.ea65a70c.css"
+  },
+  {
+    "revision": "c872af0f43aab5a2ac26",
+    "url": "/js/about.0bb40079.js"
+  },
+  {
+    "revision": "17616a4a8b1948fbdeef",
+    "url": "/css/app.743957be.css"
+  },
+  {
+    "revision": "17616a4a8b1948fbdeef",
+    "url": "/js/app.c8d9f7d0.js"
+  },
+  {
+    "revision": "36a05b35d007c4e1b049",
+    "url": "/css/chart.4011440b.css"
+  },
+  {
+    "revision": "36a05b35d007c4e1b049",
+    "url": "/js/chart.f63af5f0.js"
+  },
+  {
+    "revision": "c0e23372b6d4a022d50c",
+    "url": "/css/chart.example.5d254ab8.css"
+  },
+  {
+    "revision": "c0e23372b6d4a022d50c",
+    "url": "/js/chart.example.86f8927c.js"
+  },
+  {
+    "revision": "8ba23bdb76d108fadec8",
+    "url": "/css/chunk-vendors.3e39fceb.css"
+  },
+  {
+    "revision": "8ba23bdb76d108fadec8",
+    "url": "/js/chunk-vendors.e9e463db.js"
+  },
+  {
+    "revision": "3bc8bbcc47aeb4a6a98d",
+    "url": "/css/example1.406badca.css"
+  },
+  {
+    "revision": "3bc8bbcc47aeb4a6a98d",
+    "url": "/js/example1.059b1cbd.js"
+  },
+  {
+    "revision": "e33fb7783a9f207dbd94",
+    "url": "/css/example2.fb085f07.css"
+  },
+  {
+    "revision": "e33fb7783a9f207dbd94",
+    "url": "/js/example2.8d621c0a.js"
+  },
+  {
+    "revision": "17ed5e73c65cefa91d64",
+    "url": "/css/example3.ca108338.css"
+  },
+  {
+    "revision": "17ed5e73c65cefa91d64",
+    "url": "/js/example3.337bb4d0.js"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "6735cf32ffba8c0d2fc55ed9fefb84bc",
+    "url": "/img/back.6735cf32.png"
+  },
+  {
+    "revision": "8675979c9bfad93a2ad63792d37657d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2a65d7686b93b8fa27f20861500d461",
+    "url": "/css/fonts/iconfont.css"
+  },
+  {
+    "revision": "5c683592ac6eec6f27651099dc74506f",
+    "url": "/css/fonts/iconfont.woff2"
+  },
+  {
+    "revision": "3c3647d89326c635c71749deed02c62a",
+    "url": "/css/fonts/iconfont.woff"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/css/fonts/element-icons.woff"
+  },
+  {
+    "revision": "bca6fb0c6d76c09a979413ed9e4b4f40",
+    "url": "/home.html"
+  },
+  {
+    "revision": "c87af7afeb6a00aaa2fe7aca03033cce",
+    "url": "/img/doc/右手坐标系.jpg"
+  },
+  {
+    "revision": "f861f2afb771dcbe5bd15ad42b3a060e",
+    "url": "/img/threejs/0.png"
+  },
+  {
+    "revision": "bf11fa67d6f6224ef3d45e4b7def593e",
+    "url": "/img/threejs/2.png"
+  },
+  {
+    "revision": "ae80b3ba69e81554e20b3089f3d1780c",
+    "url": "/img/threejs/1.png"
+  },
+  {
+    "revision": "ff3648030f121ab60f713924432416b6",
+    "url": "/img/threejs/4.png"
+  },
+  {
+    "revision": "c9364e7764d71a0f1e9cc16a0039a599",
+    "url": "/img/threejs/3.png"
+  },
+  {
+    "revision": "3f98b7554c4891979807a0e9a6c23283",
+    "url": "/img/threejs/5.png"
+  },
+  {
+    "revision": "18dacf7cbfd25ee189f47a8740c34eb7",
+    "url": "/img/threejs/chess.png"
+  },
+  {
+    "revision": "a18d9a11cbd79f312087c3bcc39ee310",
+    "url": "/js/stats.min.js"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/css/fonts/element-icons.ttf"
+  },
+  {
+    "revision": "32c4cbe96334e0a36a9b144e3c191780",
+    "url": "/css/fonts/demo.css"
+  },
+  {
+    "revision": "3b59e9fbec28a4ffcd66b956598d1400",
+    "url": "/css/fonts/iconfont.eot"
+  },
+  {
+    "revision": "521bdbb30ca34c7f0e51289240c11926",
+    "url": "/css/fonts/iconfont.ttf"
+  },
+  {
+    "revision": "665c588f43825d2207a90e1ce6b86542",
+    "url": "/css/fonts/demo_index.html"
+  },
+  {
+    "revision": "ca0bf4a1e499869ea3f6517cdd82edb4",
+    "url": "/img/doc/44362685.jpg"
+  },
+  {
+    "revision": "3f3393a42dbe23ef927a511534d037ac",
+    "url": "/img/doc/frustum.jpg"
+  },
+  {
+    "revision": "105c03d1238f8941c04fb502dc48c3f8",
+    "url": "/css/fonts/iconfont.js"
+  },
+  {
+    "revision": "83b4469e99a904090c3ade38e5af1725",
+    "url": "/css/fonts/iconfont.svg"
+  },
+  {
+    "revision": "ce03a0e77817436fb024a700d6c5272c",
+    "url": "/js/dat.gui.min.js"
+  },
+  {
+    "revision": "029fd46438aa3167ef2901c2206e8709",
+    "url": "/css/download.zip"
+  },
+  {
+    "revision": "e8854a67591f481310d1a49eaeca98f6",
+    "url": "/img/2.2.jpg"
+  },
+  {
+    "revision": "ded69f51ae864ff513e38ddf3fa99c77",
+    "url": "/img/2.1.jpg"
+  },
+  {
+    "revision": "fe2824f5674cae3a792843176136431f",
+    "url": "/css/elementui.css"
+  },
+  {
+    "revision": "b0e8755b0ab71a0a4aea47c3b589b47e",
+    "url": "/js/jquery.js"
+  },
+  {
+    "revision": "8a7594187d41ad0c526dc4e56fdf049a",
+    "url": "/img/2.png"
+  },
+  {
+    "revision": "a1219f661cc099687e74ff7e35c16938",
+    "url": "/js/three.min.js"
+  },
+  {
+    "revision": "0e8f428eda7ec6d6606f8f6f2b103ab3",
+    "url": "/js/three.js"
+  },
+  {
+    "revision": "1def9981faa460e4e4529d4656f28ff8",
+    "url": "/css/fonts/SourceHanSansCN-Normal.ttf"
+  },
+  {
+    "revision": "415e506962fdedc7952572ea57cda7b8",
+    "url": "/css/Angular 打造企业级协作平台.zip"
+  },
+  {
+    "revision": "2c83235f2e5e6b686918dfcae9d66201",
+    "url": "/css/fonts/source_han_sans_cnNormal_Regular.js"
+  },
+  {
+    "revision": "966cbb577b8f8a03d1280231552ea318",
+    "url": "/css/fonts/source_han_sans_cnNormal_Regular.json"
+  }
+];

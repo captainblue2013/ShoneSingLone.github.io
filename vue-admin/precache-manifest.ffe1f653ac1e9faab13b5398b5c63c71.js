169b0f2b19bff9bcdebb905edfe1233039dd05db
ShoneSingLone <ShoneSingLone@gmail.com>
2019-12-20 00:40:39 +0800

    add vue-admin

---
+++ vue-admin/precache-manifest.ffe1f653ac1e9faab13b5398b5c63c71.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0461f8c01f6ebeed9267",
+    "url": "css/app.88b25d8e.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "4c86bcc618ebe479ce61a13e52d99844",
+    "url": "index.html"
+  },
+  {
+    "revision": "db80423ffb7a2a3712a2",
+    "url": "js/about.d9860182.js"
+  },
+  {
+    "revision": "0461f8c01f6ebeed9267",
+    "url": "js/app.78d3d103.js"
+  },
+  {
+    "revision": "5ed44fe0212354e75a6c",
+    "url": "js/chunk-vendors.fb38c3d0.js"
+  },
+  {
+    "revision": "49f211eddb9650c369ccd0ef73e689dc",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
